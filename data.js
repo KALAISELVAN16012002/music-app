b7327a7fda3e9fcfea29c88f2091f7f534fea844
KALAISELVAN16012002 <kalaitechcse37@gmail.com>
2024-03-19 09:25:00 +0530

    new

---
+++ data.js
@@ -0,0 +1,46 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: '.assets/musics/Adada-Mazhaida.mp3',
+        artist: 'artist 1',
+        cover: 'assets/images/cover 1.jpg'
+    },
+    {
+        name: 'song 2',
+        path: 'assets/musics/Life-of-Pazham-MassTamilan.dev.mp3',
+        artist: 'artist 2',
+        cover: 'assets/images/cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'assets/musics/Megham-Karukatha-MassTamilan.dev.mp3',
+        artist: 'artist 3',
+        cover: 'assets/images/cover 3.jpg'
+    },{
+        name: 'song 4',
+        path: 'assets/musics/Pacha-Elai-MassTamilan.dev.mp3',
+        artist: 'artist 4',
+        cover: 'assets/images/cover 4.png'
+    },{
+        name: 'song 5',
+        path: 'assets/musics/Patakha Guddi - Female - Highway 128 Kbps.mp3',
+        artist: 'artist 5',
+        cover: 'assets/images/cover 5.jpg'
+    },{
+        name: 'song 6',
+        path: 'assets/musics/Taal Se Taal Mila Jailer Remix-(PagalWorld).mp3',
+        artist: 'artist 6',
+        cover: 'assets/images/cover 6.jpg'
+    },{
+        name: 'song 7',
+        path: 'assets/musics/Thenmozhi-MassTamilan.dev.mp3',
+        artist: 'artist 7',
+        cover: 'assets/images/cover 7.jpg'
+    },{
+        name: 'song 8',
+        path: 'assets/musics/Life-of-Pazham-MassTamilan.dev.mp3',
+        artist: 'artist 8',
+        cover: 'assets/images/cover 8.jpg'
+    },
+    
+]
